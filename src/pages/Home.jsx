1788f74010dac9b9b20b7c4cd116cbef5bba425c
User <User@WRSD-LD309-018.dut.ac.za>
2024-09-30 11:18:22 +0200

    Updated RegisterSlice

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,20 @@ const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-50 p-6">
       <h1 className="text-4xl font-bold text-center text-blue-600 mb-4">
-        Welcome to Your Shopping List App
+        Welcome to My Shopping List App
       </h1>
       <p className="text-lg text-gray-700 mb-6 text-center">
         Easily manage your shopping lists and keep track of your items.
       </p>
       <div className="flex space-x-4">
         <a
-          href="/add-list"
+          href="/login"
           className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300"
         >
           Add New List
         </a>
         <a
-          href="/view-lists"
+          href="all"
           className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition duration-300"
         >
           View All Lists
